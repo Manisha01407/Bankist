a2c6ce90f6262f0206444671ed3de6c12adcde97
manisha ratlavath <manisharatlavath@gmail.com>
2024-05-01 19:32:28 +0530

    worked with sorting and transforming

--- script.js
+++ script.js
@@ -1,1 +1,225 @@
-console.log('hello');
+'use strict';
+{/* <div class="mx-2 md:mx-4">date</div> */}
+const movement=document.getElementById('movements');
+const bal=document.getElementById('balance');
+const inamount=document.getElementById('inamount');
+const outamount=document.getElementById('outamount');
+const intamount=document.getElementById('intamount');
+const btnlogin=document.getElementById('btnlogin');
+const inputloginusername=document.getElementById('inputloginusername');
+const inputloginpin=document.getElementById('inputloginpin');
+const welcome=document.getElementById('welcome');
+const mainblock=document.getElementById('mainblock');
+const transfortouser=document.getElementById('transfortouser');
+const amounttouser=document.getElementById('amounttouser');
+const sendamount =document.getElementById('sendamount');
+const closeuser =document.getElementById('closeuser');
+const closepin =document.getElementById('closepin');
+const closeacount =document.getElementById('closeacount');
+const reqamount =document.getElementById('reqamount');
+const reqloan =document.getElementById('reqloan');
+const sortto =document.getElementById('sortto');
+const account1={
+    owner:'Manisha Ratlavath',
+    movements:[200,450,-400,3000,-650,-130,70,1300,2000],
+    intresetRate:1.2,
+    pin:1111,
+};
+
+const account2={
+  owner:'Ajay Sangem',
+  movements:[-200,450,-400,3000,650,130,-70],
+  intresetRate:1.7,
+  pin:2222,
+};
+
+const account3={
+  owner:'Viggu mailaram',
+  movements:[20,450,-40,300,-50,-30,70,130,200],
+  intresetRate:0.8,
+  pin:3333,
+};
+
+const account4={
+  owner:'Neha Thakur',
+  movements:[250,490,-90,300,-60,130,30,300,2000],
+  intresetRate:0.2,
+  pin:4444,
+};
+
+const account=[account1,account2,account3,account4];
+
+const createusername=function(accs){
+  accs.forEach(function(acc){
+    acc.username=acc.owner.toLowerCase().split(' ').map(function(m){return m[0]}).join('');
+  });
+};
+createusername(account);
+const calcdisplaybalance=function(acc){
+  acc.balance=acc.movements.reduce(function(acc,cur,i,arr){
+    return acc+cur;
+  },0);
+  balance.textContent=`${acc.balance} $`;
+}
+
+const calcdisplaysummary=function(acc){
+  const incomebal=acc.movements.filter(function(cur){return cur>0;}).reduce(function(acc,curr){return acc+curr},0);
+  const outgoing=acc.movements.filter(function(cur){return cur<0;}).reduce(function(acc,curr){return acc+curr},0);
+  const intrest=acc.movements.filter(function(cur){return cur>0;}).map(function(m){return (m*curraccount.intresetRate)/100;}).filter(function(cur,i,arr){return cur>=1;}).reduce(function(acc,curr){return acc+curr},0);
+
+  inamount.textContent=`${incomebal}$`;
+  const ins=`<span class="text-xs text-black text-bold" id="in">IN</span> `;
+  inamount.insertAdjacentHTML('afterbegin',ins);
+
+  outamount.textContent=`${Math.abs(outgoing)}$`;
+  const out=`<span class="text-xs text-black text-bold" id="in">OUT</span> `;
+  outamount.insertAdjacentHTML('afterbegin',out);
+  
+  intamount.textContent=`${intrest}$`;
+  const int=`<span class="text-xs text-black text-bold" id="in">INT</span> `;
+  intamount.insertAdjacentHTML('afterbegin',int);
+}
+
+const displayMovements = function (movements,sort=false) {
+  movement.innerHTML='';
+
+  const movs= sort ? movements.slice().sort((a,b) => a-b): movements;
+
+  movs.forEach(function(mov,i){
+      const t = mov > 0 ? 'green' : 'red';
+      const t1 = mov > 0 ? 'deposite' : 'withdraw';
+      const html=`
+      <li class="pt-1 border border-1 border-b-zink text-xs sm:text-sm md:text-lg ">
+          <div class="flex justify-between p-1 md:p-2">
+            <div class="flex px-1">
+              <div class="mx-2 md:mx-4  px-2 rounded-lg md:px-4 md:rounded-xl bg-${t}-400 text-white"> ${i+1} ${t1}</div>
+
+            </div>
+            <div class="px-1">${mov}$</div>
+          </div>
+      </li>
+      `;
+      movement.insertAdjacentHTML('afterbegin',html);
+  });
+}
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcdisplaybalance(acc);
+
+  // Display summary
+  calcdisplaysummary(acc);
+};
+
+
+let curraccount;
+btnlogin.addEventListener('click',function(e){
+    e.preventDefault();
+
+    curraccount=account.find(function(acc){return acc.username===inputloginusername.value});
+    if(curraccount?.pin===Number(inputloginpin.value)){
+      
+      welcome.textContent=`welcome back , ${curraccount.owner.split(' ')[0]}`;
+      mainblock.style.opacity=100;
+      inputloginusername.value=inputloginpin.value='';
+      inputloginpin.blur();
+      updateUI(curraccount);
+    }
+});
+
+closeacount.addEventListener('click',function(e){
+  e.preventDefault();
+  if(curraccount.username===closeuser.value && curraccount.pin===Number(closepin.value)){
+  
+    const index=account.findIndex(function(acc){
+      return acc.username===curraccount.username;
+    });
+    account.splice(index,1);
+    mainblock.style.opacity=0;
+  }
+  closepin.value=closeuser.value='';
+  welcome.textContent=`Login to get started`;
+})
+
+sendamount.addEventListener('click',function(e){
+  e.preventDefault();
+  let fi=account.find(function(cur){
+    return cur.username===transfortouser.value;
+  });
+  if(curraccount.balance>=Number(amounttouser.value) && fi && amounttouser.value>0 && fi?.username !== curraccount.username){
+    curraccount.movements.push(-amounttouser.value);
+    fi.movements.push(Number(amounttouser.value));
+    updateUI(curraccount);
+  }
+  transfortouser.value=amounttouser.value='';
+})
+
+reqloan.addEventListener('click',function(e){
+  e.preventDefault();
+  const amount=Number(reqamount.value);
+  if(amount>0 && curraccount.movements.some(function(mov){
+    return mov>=amount*0.1;
+  })){
+    curraccount.movements.push(amount);
+    updateUI(curraccount);
+  }
+  reqamount.value='';
+})
+
+let sort=false;
+sortto.addEventListener('click',function(e){
+  e.preventDefault();
+  displayMovements(curraccount.movements,!sort);
+  sort=!sort;
+});
+
+
+// calculating overal balance in all accounts
+const overalbalance=account
+      .map(curr=>curr.movements)
+      .flat()
+      .reduce((acc,cur)=>acc+cur,0);
+console.log(overalbalance);   //13800
+
+
+
+
+const move=[-130,20,3000,1500,15,2,-17,-4];
+// equality
+console.log(move.includes(-130)); //true
+console.log(move.some(function(mov){
+  return mov===-130;
+})) //true 
+const any=move.some(function(mov){
+  return mov>0;
+})
+console.log(any);   //true
+console.log(move.every(function(mov){
+  return mov>0;
+}))   //false
+
+//flat and flatmap
+
+const arr=[[1,2,3],4,5,[6,7,8]];
+console.log(arr.flat());  //(8) [1, 2, 3, 4, 5, 6, 7, 8]
+const arr1=[[[1,2],3],4,5,[6,7,[8,9,10]],11];
+console.log(arr1.flat(2));  //(11) [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+// sorting
+// ascending order
+move.sort((a,b)=>{
+  if(a>b) return 1;
+  if(a<b) return -1;
+})
+// move.sort((a,b) => a-b)
+console.log(move);
+// decending order
+move.sort((a,b)=>{
+  if(a>b) return -1
+  if(a<b) return 1;
+})
+// move.sort((a,b) => b-a)
+console.log(move);
